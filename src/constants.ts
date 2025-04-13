9dc9bd0876ac55b51b92d4ee1d677567f921fa9c
Gorniaky <hwss.s2@gmail.com>
2025-04-13 17:52:50 -0300

    add MAX_ZIP_SIZE constant and improve ZIP size validation

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+/** 100MB */
+export const MAX_ZIP_SIZE = 104_857_600;
--- src/zip.ts
+++ src/zip.ts
@@ -1,6 +1,7 @@
 import { debug, notice } from "@actions/core";
 import { getExecOutput } from "@actions/exec";
 import bytes from "bytes";
+import { MAX_ZIP_SIZE } from "./constants";
 
 export default async function zip(glob?: string | string[]) {
   if (Array.isArray(glob)) glob = glob.join(" ");
@@ -23,7 +24,10 @@ export default async function zip(glob?: string | string[]) {
 
   const buffer = Buffer.from(parts[parts[0].includes(zipCommand) ? 1 : 0], encoding);
 
-  notice(`Zip size: ${bytes(buffer.length)}`);
+  if (buffer.length > MAX_ZIP_SIZE)
+    throw new Error(`The ZIP size cannot exceed 100 MB (1048576 KB). ZIP size: ${bytes(buffer.length)}`);
+
+  notice(`ZIP size: ${bytes(buffer.length)}`);
 
   return buffer;
 }
