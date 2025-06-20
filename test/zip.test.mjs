940d095e9fe9d4d462db41d1f130f84f1f5a35ed
Gorniaky <hwss.s2@gmail.com>
2025-06-20 14:30:52 -0300

    Refactor zip test to use consistent module naming

--- test/zip.test.mjs
+++ test/zip.test.mjs
@@ -1,15 +1,14 @@
 import test, { suite } from "node:test";
-import module from "../out/zip.js";
+import zipModule from "../out/zip.js";
 
-/** @type {typeof module} */
-const zip = module.default;
+/** @type {typeof zipModule} */
+const zip = zipModule.default;
 
 suite("ZIP", async () => {
   await test("Testing empty zip", async (t) => {
     const glob = `__not_expected_files__${Math.random()}`;
     const expectedBufferSize = 22;
 
-    /** @type {Buffer} */
     const buffer = await zip(glob);
 
     t.assert.strictEqual(buffer.length, expectedBufferSize);
